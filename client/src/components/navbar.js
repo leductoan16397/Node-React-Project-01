d57fc9c04cec239de7bc5da2fa9a95755d2f7f7f
Le Duc Toan <toan.le@vn.devinition.com>
2020-08-06 17:52:40 +0700

    dev - commit

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,32 +1,95 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import classNames from 'classnames'
 
-export default function NvarBar() {
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+    },
+    buttonStyle: {
+        fontSize: '12px'
+    },
+    toolbar: {
+        background: 'white',
+        margin: 'auto',
+        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
+    },
+    toolbarNoShadow: {
+        background: 'white',
+        margin: 'auto',
+    }
+}));
+
+export default function NvarBar(props) {
+    const classes = useStyles();
+    let styleToolbar = classes.toolbar;
     const [logoSrc, setLogoSrc] = useState();
-
+    const color = 'transparent';
     useEffect(() => {
-        setLogoSrc("/static/images/logo.png");
-    },[])
+        setLogoSrc("/static/karma/img/logo/logo.png");
+    }, [])
+    function ElevationScroll(props) {
+        const { children, window } = props;
+        // Note that you normally won't need to set the window ref as useScrollTrigger
+        // will default to window.
+        // This is only being set here because the demo is in an iframe.
+        const trigger = useScrollTrigger({
+            disableHysteresis: true,
+            threshold: 0,
+            target: window ? window() : undefined,
+        });
+
+        console.log(styleToolbar);
+        return React.cloneElement(children, {
+            elevation: trigger ? 4 : 0,
+            color: trigger ? 'inherit' : 'transparent',
+        });
+    }
+
+    ElevationScroll.propTypes = {
+        children: PropTypes.element.isRequired,
+        window: PropTypes.func,
+    };
 
     return (
-        <div className="navbar navbar-expand-md navbar-light bg-light">
-            <a href="#" className="navbar-brand">
-                <img src={logoSrc} height={28} alt="CoolBrand" />
-            </a>
-            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <div className="navbar-nav">
-                    <a href="#" className="nav-item nav-link active">Home</a>
-                    <a href="#" className="nav-item nav-link">Profile</a>
-                    <a href="#" className="nav-item nav-link">Messages</a>
-                    <a href="#" className="nav-item nav-link disabled" tabIndex={-1}>Reports</a>
-                </div>
-                <div className="navbar-nav ml-auto">
-                    <a href="#" className="nav-item nav-link">Login</a>
-                </div>
-            </div>
-        </div>
+        <React.Fragment >
+            <CssBaseline />
+            <ElevationScroll >
+                <AppBar className={classes.root} color={color}>
+                    <Toolbar className={classNames(classes.toolbar, 'container')} >
+                        <div>
+                            <img src={logoSrc} alt='' />
+                        </div>
+                        <div style={{ flexGrow: 1 }} className='row'>
+                            <div style={{ marginLeft: 'auto' }}>
+                                <Button className={classes.buttonStyle} color="inherit">HOME</Button>
+                                <Button className={classes.buttonStyle} color="inherit">SHOP</Button>
+                                <Button className={classes.buttonStyle} color="inherit">BLOG</Button>
+                                <Button className={classes.buttonStyle} color="inherit">PAGES</Button>
+                                <Button className={classes.buttonStyle} color="inherit">CONTACT</Button>
+                            </div>
+                            <div>
+                                <IconButton aria-label="Cart" color="inherit">
+                                    <ShoppingCartIcon />
+                                </IconButton>
+                                <IconButton aria-label="search" color="inherit">
+                                    <SearchIcon />
+                                </IconButton>
+                            </div>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+        </React.Fragment>
     );
 }
