1353c967dbf420ec751a8af9538152fee7dc65ae
Le Duc Toan <toan.le@vn.devinition.com>
2020-08-07 16:18:00 +0700

    dev - commit

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,95 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import classNames from 'classnames'
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-    },
-    buttonStyle: {
-        fontSize: '12px'
-    },
-    toolbar: {
-        background: 'white',
-        margin: 'auto',
-        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
-    },
-    toolbarNoShadow: {
-        background: 'white',
-        margin: 'auto',
-    }
-}));
-
-export default function NvarBar(props) {
-    const classes = useStyles();
-    let styleToolbar = classes.toolbar;
+export default function NvarBar() {
     const [logoSrc, setLogoSrc] = useState();
-    const color = 'transparent';
     useEffect(() => {
         setLogoSrc("/static/karma/img/logo/logo.png");
     }, [])
-    function ElevationScroll(props) {
-        const { children, window } = props;
-        // Note that you normally won't need to set the window ref as useScrollTrigger
-        // will default to window.
-        // This is only being set here because the demo is in an iframe.
-        const trigger = useScrollTrigger({
-            disableHysteresis: true,
-            threshold: 0,
-            target: window ? window() : undefined,
-        });
-
-        console.log(styleToolbar);
-        return React.cloneElement(children, {
-            elevation: trigger ? 4 : 0,
-            color: trigger ? 'inherit' : 'transparent',
-        });
-    }
-
-    ElevationScroll.propTypes = {
-        children: PropTypes.element.isRequired,
-        window: PropTypes.func,
-    };
 
     return (
-        <React.Fragment >
-            <CssBaseline />
-            <ElevationScroll >
-                <AppBar className={classes.root} color={color}>
-                    <Toolbar className={classNames(classes.toolbar, 'container')} >
-                        <div>
-                            <img src={logoSrc} alt='' />
-                        </div>
-                        <div style={{ flexGrow: 1 }} className='row'>
-                            <div style={{ marginLeft: 'auto' }}>
-                                <Button className={classes.buttonStyle} color="inherit">HOME</Button>
-                                <Button className={classes.buttonStyle} color="inherit">SHOP</Button>
-                                <Button className={classes.buttonStyle} color="inherit">BLOG</Button>
-                                <Button className={classes.buttonStyle} color="inherit">PAGES</Button>
-                                <Button className={classes.buttonStyle} color="inherit">CONTACT</Button>
+        <header>
+            <div className="header-area ">
+                <div className="header-top_area">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-8">
+                                <div className="short_contact_list">
+                                    <ul>
+                                        <li><a href="#">+880 4664 216</a></li>
+                                        <li><a href="#">Mon - Sat 10:00 - 7:00</a></li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div>
-                                <IconButton aria-label="Cart" color="inherit">
-                                    <ShoppingCartIcon />
-                                </IconButton>
-                                <IconButton aria-label="search" color="inherit">
-                                    <SearchIcon />
-                                </IconButton>
+                            <div className="col-lg-6 col-md-4 ">
+                                <div className="social_media_links">
+                                    <a href="#">
+                                        <i className="fa fa-facebook" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fa fa-pinterest-p" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fa fa-google-plus" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fa fa-linkedin" />
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </Toolbar>
-                </AppBar>
-            </ElevationScroll>
-            <Toolbar />
-        </React.Fragment>
+                    </div>
+                </div>
+                <div id="sticky-header" className="main-header-area">
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col-xl-3 col-lg-3">
+                                <div className="logo">
+                                    <a href="index.html">
+                                        <img src="logo.png" alt="" />
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="col-xl-9 col-lg-9">
+                                <div className="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.html">home</a></li>
+                                            <li><a href="about.html">about</a></li>
+                                            <li><a href="#">blog <i className="ti-angle-down" /></a>
+                                                <ul className="submenu">
+                                                    <li><a href="blog.html">blog</a></li>
+                                                    <li><a href="single-blog.html">single-blog</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#">pages <i className="ti-angle-down" /></a>
+                                                <ul className="submenu">
+                                                    <li><a href="elements.html">elements</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="service.html">services</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="mobile_menu d-block d-lg-none" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
     );
 }
